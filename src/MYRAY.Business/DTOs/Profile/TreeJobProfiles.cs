875e240a3feaef8e923720d57b1d1e25b7212256
HL112 <forest.tl112@gmail.com>
2022-06-27 04:08:20 +0700

    Fix Detail Tre Job

--- src/MYRAY.Business/DTOs/Profile/TreeJobProfiles.cs
+++ src/MYRAY.Business/DTOs/Profile/TreeJobProfiles.cs
@@ -9,7 +9,10 @@ public static class TreeJobProfiles
 {
     public static void ConfigTreeJob(this IMapperConfigurationExpression configuration)
     {
-        configuration.CreateMap<DataTier.Entities.TreeJob, TreeJobDetail>().ReverseMap();
+        configuration.CreateMap<DataTier.Entities.TreeJob, TreeJobDetail>()
+            .ForMember(des => des.Type,
+                opt => opt.MapFrom(src => src.TreeType.Type))
+            .ReverseMap();
         configuration.CreateMap<DataTier.Entities.TreeJob, CreateTreeJob>().ReverseMap();
         configuration.CreateMap<DataTier.Entities.TreeJob, DeleteTreeJob>().ReverseMap();
     }
