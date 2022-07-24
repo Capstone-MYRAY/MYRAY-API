2add8ae312f6a8b5b30c10d896489c5c443fdee0
Forest <forest.tl112@gmail.com>
2022-07-24 21:30:48 +0700

    Add Reason for attendance

--- src/MYRAY.Business/DTOs/Attendance/CheckAttendance.cs
+++ src/MYRAY.Business/DTOs/Attendance/CheckAttendance.cs
@@ -11,5 +11,6 @@ public class CheckAttendance
     public DateTime DateAttendance { get; set; }
     public int AccountId { get; set; }
     public string? Signature { get; set; }
+    public string? Reason { get; set; }
     public AttendanceEnum.AttendanceStatus Status { get; set; }
 }
