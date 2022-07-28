19fac1fec3747db1aa39c149652ccf0bdf7e362e
Forest <forest.tl112@gmail.com>
2022-07-29 00:55:55 +0700

    Edit Dto Guidepost

--- src/MYRAY.Business/DTOs/Guidepost/GuidepostDetail.cs
+++ src/MYRAY.Business/DTOs/Guidepost/GuidepostDetail.cs
@@ -5,7 +5,7 @@ public class GuidepostDetail
     public int Id { get; set; }
     public string? Title { get; set; }
     public string? Content { get; set; }
-    public DateTime CreateDate { get; set; }
-    public DateTime? UpdateDate { get; set; }
-    public int CreateBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public int CreatedBy { get; set; }
 }
